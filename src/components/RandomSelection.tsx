caf5faa87f6b33c127966e3a32e364ca2a2b5647
enlu <nicholas.lugo22@gmail.com>
2025-05-06 08:00:15 -0600

    Adds feature to have multiple rosters and switch between them

--- src/components/RandomSelection.tsx
+++ src/components/RandomSelection.tsx
@@ -8,255 +8,266 @@ const RandomSelection = () => {
   /* ROSTER VARIABLES */
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
   const [roster, setRoster] = useState<string[]>([]);
-  const [sortedRoster, setSortedRoster] = useState<string[]>([]);
-  const [absent, setAbsent] = useState<string[]>([]);
-  const [selectedNames, setSelectedNames] = useState<string[]>([]);
-  const [numAvailableNames, setNumAvailableNames] = useState(0); // used to set select element for max num of spinners
-  const [isNumbered, setIsNumbered] = useState<boolean>(false);
-  const [isSorted, setIsSorted] = useState<boolean>(false);
-  const [isRosterLoaded, setIsRosterLoaded] = useState<boolean>(false);
-  const [isRosterDisplayed, setIsRosterDisplayed] = useState<boolean>(false);
+const [selectedRoster, setSelectedRoster] = useState<string>("Roster");
+const [sortedRoster, setSortedRoster] = useState<string[]>([]);
+const [absentMap, setAbsentMap] = useState<Record<string, string[]>>({});
+const [selectedNames, setSelectedNames] = useState<string[]>([]);
+const [numAvailableNames, setNumAvailableNames] = useState(0); // used to set select element for max num of spinners
+const [isNumbered, setIsNumbered] = useState<boolean>(false);
+const [isSorted, setIsSorted] = useState<boolean>(false);
+const [isRosterLoaded, setIsRosterLoaded] = useState<boolean>(false);
+const [isRosterDisplayed, setIsRosterDisplayed] = useState<boolean>(false);
 
-  /* SPINNER VARIABLES */
-  const [isSpinning, setIsSpinning] = useState(false);
-  const [spinnerCount, setSpinnerCount] = useState(1); // num of spinners on the screen
-  const [spinnerNames, setSpinnerNames] = useState<string[]>([]); // manages the names assigned on each spinner
+/* SPINNER VARIABLES */
+const [isSpinning, setIsSpinning] = useState(false);
+const [spinnerCount, setSpinnerCount] = useState(1); // num of spinners on the screen
+const [spinnerNames, setSpinnerNames] = useState<string[]>([]); // manages the names assigned on each spinner
 
-  /* load save data on component mount */
-  useEffect(() => {
-    const savedRoster = localStorage.getItem("roster");
-    const savedAbsent = localStorage.getItem("absent");
-    const savedSelectedNames = localStorage.getItem("selectedNames");
-    const savedSpinnerNames = localStorage.getItem("spinnerNames");
-    if (savedRoster && !isRosterLoaded) {
-      setRoster(JSON.parse(savedRoster));
-      setIsRosterLoaded(true);
-      setIsRosterDisplayed(true);
-    }
-
-    if (savedAbsent) {
-      setAbsent(JSON.parse(savedAbsent));
-    }
-
-    if (savedSelectedNames) {
-      setSelectedNames(JSON.parse(savedSelectedNames));
-    }
-
-    if (savedSpinnerNames) {
-      setSpinnerNames(JSON.parse(savedSpinnerNames));
-      setSpinnerCount(JSON.parse(savedSpinnerNames).length);
-    }
-  }, [isRosterLoaded]);
-
-  /* save names and roster */
-  useEffect(() => {
-    if (isRosterLoaded) {
-      localStorage.setItem("roster", JSON.stringify(roster));
-      localStorage.setItem("absent", JSON.stringify(absent));
-      localStorage.setItem("selectedNames", JSON.stringify(selectedNames));
-      localStorage.setItem("spinnerNames", JSON.stringify(spinnerNames));
-      localStorage.setItem("spinCount", JSON.stringify(spinnerCount));
-    }
-  }, [roster, absent, selectedNames, isRosterLoaded]);
-
-  /*******************/
-  /* ROSTER SECTION */
-  /*******************/
-  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0];
-    setSelectedFile(file || null);
-  };
-
-  const handleFileUpload = () => {
-    if (!selectedFile) {
-      window.alert("No file input!");
-      return;
-    }
-
-    /* READ THE FILE AND SET THE NAMES INTO ROSTER  */
-    const reader = new FileReader();
-    reader.onload = (e: ProgressEvent<FileReader>) => {
-      const fileContent = e.target?.result;
-      if (typeof fileContent === "string") {
-        const names = fileContent
-          .split("\n")
-          .map((name) => name.trim())
-          .filter((name) => name);
-        setRoster(names);
-      }
-    };
-
-    reader.onerror = () => {
-      alert("Error reading file, try again!");
-    };
-    // the roster is successfully uploaded
-    reader.readAsText(selectedFile);
+/* load save data on component mount */
+useEffect(() => {
+  const savedRoster = localStorage.getItem("roster");
+  const savedAbsent = localStorage.getItem("absentMap");
+  const savedSelectedNames = localStorage.getItem("selectedNames");
+  const savedSpinnerNames = localStorage.getItem("spinnerNames");
+  if (savedRoster && !isRosterLoaded) {
+    setRoster(JSON.parse(savedRoster));
     setIsRosterLoaded(true);
     setIsRosterDisplayed(true);
-  };
-
-  /* set number of names after the roster is set used for select element */
-  useEffect(() => {
-    setNumAvailableNames(
-      roster.filter(
-        (name) => !selectedNames.includes(name) && !absent.includes(name),
-      ).length,
-    );
-  }, [roster, selectedNames, absent]);
-
-  useEffect(() => {
-    const sorted = [...roster].sort()
-    setSortedRoster(sorted)
-  }, [roster])
-
-  const handleRosterDisplayed = () => setIsRosterDisplayed((prev) => !prev);
-  const handleIsNumbered = () => setIsNumbered((prev) => !prev);
-
-  const handleSort = () => {
-    setIsSorted((prev) => !prev)
-  };
-
-  /* Handle absent students */
-  const handleAbsent = (updatedAbsent: string[]) => {
-    setAbsent(updatedAbsent);
-  };
-
-  /*******************/
-  /* SPINNER SECTION */
-  /*******************/
-  const handleSpinnerCountChange = (newCount: number) => {
-    if (isSpinning) return;
-
-    setSpinnerCount(newCount);
-    setSpinnerNames([]);
-  };
-
-  const handleSpinning = () => {
-    const availableNamesCount = roster.filter(
-      (name) => !selectedNames.includes(name) && !absent.includes(name),
-    ).length;
-    if (isSpinning || spinnerCount > availableNamesCount) return;
-
-    setIsSpinning(true);
-    const tempSpinnerNames = Array(spinnerCount).fill("");
-    const intervals: number[] = [];
-
-    /* Start spinning all spinners */
-    tempSpinnerNames.forEach((_, index) => {
-      let tempIdx = index % roster.length;
-      const interval = setInterval(() => {
-        tempSpinnerNames[index] = roster[tempIdx];
-        tempIdx = (tempIdx + 1) % roster.length;
-        setSpinnerNames([...tempSpinnerNames]);
-      }, 100);
-      intervals.push(interval);
-    });
-
-    /* Stop all spinners */
-    setTimeout(() => {
-      intervals.forEach(clearInterval);
-
-      const availableNames: string[] = roster.filter(
-        (name) => !selectedNames.includes(name) && !absent.includes(name),
-      );
-      const finalNames: string[] = [];
-
-      for (let i = 0; i < spinnerCount; i++) {
-        const randomIndex = Math.floor(Math.random() * availableNames.length);
-        finalNames.push(availableNames.splice(randomIndex, 1)[0]); // pushes the chosen name into final and removes from available names
-      }
-
-      setSpinnerNames(finalNames);
-      setSelectedNames((prevNames) => [...prevNames, ...finalNames]);
-      setIsSpinning(false);
-
-      /* Adjust spinner count */
-      const remainingNamesCount = roster.length - selectedNames.length;
-      if (spinnerCount > remainingNamesCount) {
-        setSpinnerCount(remainingNamesCount);
-      }
-    }, 3000);
-  };
-
-  const handleReset = () => {
-    setSpinnerNames([]);
-    setSelectedNames([]);
-  };
-
-  const handleClearAbsent = () => {
-    setAbsent([]);
   }
 
-  /* RESET EVERYTHING FOR NEW ROSTER UPLOAD */
-  const handleGoBack = () => {
-    localStorage.removeItem("roster");
-    localStorage.removeItem("absent");
-    localStorage.removeItem("selectedNames");
+  if (savedAbsent) {
+    setAbsentMap(JSON.parse(savedAbsent));
+  }
 
-    setSelectedFile(null);
-    setRoster([]);
-    setIsRosterDisplayed(false);
-    setIsRosterLoaded(false);
-    setSelectedNames([]);
-    setSpinnerNames([]);
-    setSpinnerCount(1);
-    setAbsent([]);
+  if (savedSelectedNames) {
+    setSelectedNames(JSON.parse(savedSelectedNames));
+  }
+
+  if (savedSpinnerNames) {
+    setSpinnerNames(JSON.parse(savedSpinnerNames));
+    setSpinnerCount(JSON.parse(savedSpinnerNames).length);
+  }
+}, [isRosterLoaded]);
+
+/* save names and roster */
+useEffect(() => {
+  if (isRosterLoaded) {
+    localStorage.setItem("roster", JSON.stringify(roster));
+    localStorage.setItem("absentMap", JSON.stringify(absentMap));
+    localStorage.setItem("selectedNames", JSON.stringify(selectedNames));
+    localStorage.setItem("spinnerNames", JSON.stringify(spinnerNames));
+    localStorage.setItem("spinCount", JSON.stringify(spinnerCount));
+  }
+}, [roster, absentMap, selectedNames, isRosterLoaded]);
+
+/*******************/
+/* ROSTER SECTION */
+/*******************/
+const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const file = event.target.files?.[0];
+  setSelectedFile(file || null);
+};
+
+const handleFileUpload = () => {
+  if (!selectedFile) {
+    window.alert("No file input!");
+    return;
+  }
+
+  /* READ THE FILE AND SET THE NAMES INTO ROSTER  */
+  const reader = new FileReader();
+  reader.onload = (e: ProgressEvent<FileReader>) => {
+    const fileContent = e.target?.result;
+    if (typeof fileContent === "string") {
+      const names = fileContent
+        .split("\n")
+        .map((name) => name.trim())
+        .filter((name) => name);
+      setRoster(names);
+    }
   };
 
-  return (
-    <>
-      {/* UPLOAD FILE INPUT */}
-      {!isRosterLoaded && (
-        <div className="upload-container">
-          <h2>Upload Student Roster</h2>
-          <input
-            className="btn text-white"
-            type="file"
-            accept=".txt"
-            onChange={handleFileChange}
-          />
-          <button
-            className="btn btn-primary text-white"
-            onClick={handleFileUpload}
-            disabled={!selectedFile}
-          >
-            Upload
-          </button>
-        </div>
-      )}
+  reader.onerror = () => {
+    alert("Error reading file, try again!");
+  };
+  // the roster is successfully uploaded
+  reader.readAsText(selectedFile);
+  setIsRosterLoaded(true);
+  setIsRosterDisplayed(true);
+};
 
-      {/* MAIN CONTENT */}
-      {isRosterLoaded && (
-        <div className="main-container">
-          {/* CONTROLS SECTION */}
-          <RandomSelectControls
-            numAvailableNames={numAvailableNames}
-            spinnerCount={spinnerCount}
-            isRosterDisplayed={isRosterDisplayed}
-            isNumbered={isNumbered}
-            isSorted={isSorted}
-            isSpinning={isSpinning}
-            handleSpinnerCountChange={handleSpinnerCountChange}
-            handleRosterDisplayed={handleRosterDisplayed}
-            handleIsNumbered={handleIsNumbered}
-            handleSort={handleSort}
-            handleSpinning={handleSpinning}
-            handleReset={handleReset}
-            handleGoBack={handleGoBack}
-            handleClearAbsent={handleClearAbsent}
-          />
-          {/* END CONTROLS SECTION */}
+/* set number of names after the roster is set used for select element */
+useEffect(() => {
+  const absent = absentMap[selectedRoster] || [];
+  setNumAvailableNames(
+    roster.filter(
+      (name) => !selectedNames.includes(name) && !absent.includes(name),
+    ).length,
+  );
+}, [roster, selectedNames, absentMap, selectedRoster]);
 
-          {/* ROSTER AND NAME SPINNER */}
-          <div className="d-flex justify-content-between">
-            {/* <div className="col-1"> */}
-            {roster.length > 0 && isRosterDisplayed && (
-              <div className="me-auto">
-                <Roster
+useEffect(() => {
+  const sorted = [...roster].sort()
+  setSortedRoster(sorted)
+}, [roster])
+
+const handleRosterDisplayed = () => setIsRosterDisplayed((prev) => !prev);
+const handleIsNumbered = () => setIsNumbered((prev) => !prev);
+
+const handleSort = () => {
+  setIsSorted((prev) => !prev)
+};
+
+/* Handle absent students */
+// const handleAbsent = () => {
+//   setAbsentMap((prev) => ({
+//     ...prev, [selectedRoster]: updatedAbsent,
+//   }));
+// };
+
+/*******************/
+/* SPINNER SECTION */
+/*******************/
+const handleSpinnerCountChange = (newCount: number) => {
+  if (isSpinning) return;
+
+  setSpinnerCount(newCount);
+  setSpinnerNames([]);
+};
+
+const handleSpinning = () => {
+  const absent = absentMap[selectedRoster];
+  const availableNamesCount = roster.filter(
+    (name) => !selectedNames.includes(name) && !absent.includes(name),
+  ).length;
+  if (isSpinning || spinnerCount > availableNamesCount) return;
+
+  setIsSpinning(true);
+  const tempSpinnerNames = Array(spinnerCount).fill("");
+  const intervals: number[] = [];
+
+  /* Start spinning all spinners */
+  tempSpinnerNames.forEach((_, index) => {
+    let tempIdx = index % roster.length;
+    const interval = setInterval(() => {
+      tempSpinnerNames[index] = roster[tempIdx];
+      tempIdx = (tempIdx + 1) % roster.length;
+      setSpinnerNames([...tempSpinnerNames]);
+    }, 100);
+    intervals.push(interval);
+  });
+
+  /* Stop all spinners */
+  setTimeout(() => {
+    intervals.forEach(clearInterval);
+
+    const availableNames: string[] = roster.filter(
+      (name) => !selectedNames.includes(name) && !absent.includes(name),
+    );
+    const finalNames: string[] = [];
+
+    for (let i = 0; i < spinnerCount; i++) {
+      const randomIndex = Math.floor(Math.random() * availableNames.length);
+      finalNames.push(availableNames.splice(randomIndex, 1)[0]); // pushes the chosen name into final and removes from available names
+    }
+
+    setSpinnerNames(finalNames);
+    setSelectedNames((prevNames) => [...prevNames, ...finalNames]);
+    setIsSpinning(false);
+
+    /* Adjust spinner count */
+    const remainingNamesCount = roster.length - selectedNames.length;
+    if (spinnerCount > remainingNamesCount) {
+      setSpinnerCount(remainingNamesCount);
+    }
+  }, 3000);
+};
+
+const handleReset = () => {
+  setSpinnerNames([]);
+  setSelectedNames([]);
+};
+
+const handleClearAbsent = () => {
+  setAbsentMap(prev => {
+    const updated = { ...prev};
+    delete updated[selectedRoster];
+    return updated;
+  });
+}
+
+/* RESET EVERYTHING FOR NEW ROSTER UPLOAD */
+const handleGoBack = () => {
+  localStorage.removeItem("roster");
+  localStorage.removeItem("absentMap");
+  localStorage.removeItem("selectedNames");
+
+  setSelectedFile(null);
+  setRoster([]);
+  setIsRosterDisplayed(false);
+  setIsRosterLoaded(false);
+  setSelectedNames([]);
+  setSpinnerNames([]);
+  setSpinnerCount(1);
+  setAbsentMap({});
+};
+
+return (
+  <>
+    {/* UPLOAD FILE INPUT */}
+    {!isRosterLoaded && (
+      <div className="upload-container">
+        <h2>Upload Student Roster</h2>
+        <input
+          className="btn text-white"
+          type="file"
+          accept=".txt"
+          onChange={handleFileChange}
+        />
+        <button
+          className="btn btn-primary text-white"
+          onClick={handleFileUpload}
+          disabled={!selectedFile}
+        >
+          Upload
+        </button>
+      </div>
+    )}
+
+    {/* MAIN CONTENT */}
+    {isRosterLoaded && (
+      <div className="main-container">
+        {/* CONTROLS SECTION */}
+        <RandomSelectControls
+          numAvailableNames={numAvailableNames}
+          spinnerCount={spinnerCount}
+          isRosterDisplayed={isRosterDisplayed}
+          isNumbered={isNumbered}
+          isSorted={isSorted}
+          isSpinning={isSpinning}
+          handleSpinnerCountChange={handleSpinnerCountChange}
+          handleRosterDisplayed={handleRosterDisplayed}
+          handleIsNumbered={handleIsNumbered}
+          handleSort={handleSort}
+          handleSpinning={handleSpinning}
+          handleReset={handleReset}
+          handleGoBack={handleGoBack}
+          handleClearAbsent={handleClearAbsent}
+        />
+        {/* END CONTROLS SECTION */}
+
+        {/* ROSTER AND NAME SPINNER */}
+        <div className="d-flex justify-content-between">
+          {/* <div className="col-1"> */}
+          {roster.length > 0 && isRosterDisplayed && (
+            <div className="me-auto">
+              <Roster
                   isNumbered={isNumbered}
                   roster={isSorted ? sortedRoster : roster}
-                  absent={absent}
-                  setAbsent={handleAbsent}
+                  rosterName={selectedRoster}
+                  absentMap={absentMap}
+                  setAbsentMap={setAbsentMap}
+                  onRosterChange={setSelectedRoster}
                 />
               </div>
             )}
--- src/components/Roster.tsx
+++ src/components/Roster.tsx
@@ -1,24 +1,182 @@
+import { useEffect, useState } from "react";
 import "../styles/Roster.css";
 
 interface Props {
   isNumbered: boolean;
   roster: string[];
-  absent: string[];
-  setAbsent: (absentList: string[]) => void;
+  rosterName: string;
+  absentMap: Record<string, string[]>;
+  setAbsentMap: (map: Record<string, string[]>) => void;
+  onRosterChange: (newRoster: string) => void;
 }
 
-const Roster = ({ isNumbered, roster, absent, setAbsent }: Props) => {
-  const handleClick = (name: string) => {
-    const updatedAbsent = absent.includes(name)
-      ? absent.filter((n) => n !== name)
-      : [...absent, name];
+const Roster = ({ isNumbered, roster, rosterName, absentMap, setAbsentMap, onRosterChange }: Props) => {
+  const [showMenu, setShowMenu] = useState<boolean>(false);
+  const [currentRosterName, setCurrentRosterName] = useState<string>("Roster");
+  const [rosterList, setRosterList] = useState<string[]>(["Roster"]);
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+  const [nameToEdit, setNameToEdit] = useState<string>("");
+  const [newName, setNewName] = useState<string>("");
+  const [addName, setAddName] = useState<string>("");
+  const [errorMessage, setErrorMessage] = useState<string>("");
 
-    setAbsent(updatedAbsent);
+  useEffect(() => {}, [rosterList]);
+
+  const handleClick = (name: string) => {
+    const currentAbsent = absentMap[rosterName] || [];
+    let updatedAbsent: string[];
+
+    if (currentAbsent.includes(name)) {
+      updatedAbsent = currentAbsent.filter((n) => n !== name);
+    } else {
+      updatedAbsent = [...currentAbsent, name];
+    }
+
+    setAbsentMap({
+      ...absentMap,
+      [rosterName]: updatedAbsent,
+    });
+  };
+
+  const addItems = () => {
+    if (!addName) {
+      setErrorMessage("You need to enter a name!");
+    } else if (rosterList.includes(addName)) {
+      setErrorMessage("You need to enter a unique name!");
+    } else {
+      const updatedNames = [...rosterList, addName];
+      setRosterList(updatedNames);
+      setErrorMessage("");
+    }
+  };
+
+  const editItems = () => {
+    if (newName && !rosterList.includes(newName)) {
+      const updatedList = rosterList.map((name) =>
+        name === nameToEdit ? newName : name,
+      );
+
+      setRosterList(updatedList);
+    }
+
+    setIsEditing(false);
+    setCurrentRosterName(newName);
+    setNameToEdit("");
+    setNewName("");
+  };
+
+  const deleteItems = (rosterToDelete: string) => {
+    if (rosterList.length === 1) {
+      alert("At least one item must remain.");
+      return;
+    }
+
+    const newRosterList = rosterList.filter(
+      (rosterItem) => rosterItem !== rosterToDelete,
+    );
+    setRosterList(newRosterList);
+
+    if (currentRosterName === rosterToDelete) {
+      setCurrentRosterName(newRosterList[0]);
+    }
   };
 
   return (
     <div className="roster-container">
-      <h3 className="roster-title text-white bg-dark">Roster</h3>
+      {/* The header */}
+      <div className="roster-title">
+        <button
+          className="border-0 bg-transparent text-white fs-6"
+          onClick={() => setShowMenu((prev) => !prev)}
+        >
+          {currentRosterName} ▼
+        </button>
+
+        {/* The list of roster names */}
+      </div>
+      {showMenu && (
+        <div>
+          {rosterList.map((roster) =>
+            // Show edit input and save if editing
+            isEditing && nameToEdit === roster ? (
+              <div key={roster} className="d-flex align-items-center gap-2">
+                <input
+                  type="text"
+                  value={newName}
+                  onChange={(e) => setNewName(e.target.value)}
+                  placeholder="Enter item name."
+                  className="form-control form-control-sm"
+                />
+                <button
+                  className="btn btn-sm btn-success"
+                  onClick={() => {
+                    editItems();
+                  }}
+                >
+                  Save
+                </button>
+              </div>
+            ) : (
+              // if not editing, show item, edit and delete button
+              <div>
+                <span
+                  className="flex-grow-1"
+                  style={{ cursor: "pointer" }}
+                  onClick={() => {
+                    setCurrentRosterName(roster);
+                    setShowMenu(false);
+                    onRosterChange(roster);
+                  }}
+                >
+                  {roster}
+                </span>
+                <button
+                  className="btn btn-sm btn-outline-light"
+                  onClick={() => {
+                    setIsEditing(true);
+                    setNameToEdit(roster);
+                    setNewName(roster);
+                  }}
+                >
+                  ✏️
+                </button>
+                <button
+                  className="btn btn-sm btn-outline-light"
+                  onClick={() => deleteItems(roster)}
+                >
+                  🗑️
+                </button>
+              </div>
+            ),
+          )}
+
+          {/* Add an item */}
+          <div className="d-flex">
+            <input
+              className="form-control form-control-sm"
+              type="text"
+              value={addName}
+              onChange={(e) => setAddName(e.target.value)}
+              placeholder="New Item"
+            />
+            <button
+              onClick={() => {
+                addItems();
+                setAddName("");
+              }}
+            >
+              +
+            </button>
+          </div>
+
+          { /* Add item validation */ }
+          {errorMessage && (
+            <div className="text-danger fs-6">{errorMessage}</div>
+          )}
+        </div>
+      )}
+
+      {/* Display the names in the table */}
       <div className="table-container">
         <table className="table table-striped table-bordered">
           <thead className="table-dark">
@@ -34,7 +192,7 @@ const Roster = ({ isNumbered, roster, absent, setAbsent }: Props) => {
                   {isNumbered && <td>{index + 1}</td>}
                   <td
                     onClick={() => handleClick(name)}
-                    className={absent.includes(name) ? "bg-danger" : ""}
+                    className={(absentMap[currentRosterName] || []).includes(name) ? "bg-danger" : ""}
                   >
                     {name}
                   </td>
