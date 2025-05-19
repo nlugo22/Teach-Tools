4df0cebe6cad0509f7a06db663ea7b4fc62e0215
enlu <nicholas.lugo22@gmail.com>
2025-05-18 22:01:52 -0600

    Adds a favicon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,16 @@
+import favicon from "/src/assets/favicon.ico";
+
 const Navbar = () => {
   return (
     <header className="bg-gray-800 text-white text-sm">
       <nav className="px-4 py-2 flex gap-6">
-        <button onClick={() => {}} className="hover:text-blue-300">
-          Home
-        </button>
+        <img
+          src={favicon}
+          alt="Home"
+          title="Home"
+          onClick={() => {}}
+          className="hover:brightness-75 w-9 h-9"
+        />
         <button onClick={() => {}} className="hover:text-blue-300">
           About
         </button>
